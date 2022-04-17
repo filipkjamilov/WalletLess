2bc8b3a0d21d350e4047fe6bba01a7d41862cd00
Filip Kjamilov <filip.kjamilov@netcetera.com>
2022-04-17 14:11:20 +0200

    [no-issue] ADD initial settings design

--- BachelorThesis/views/Settings.swift
+++ BachelorThesis/views/Settings.swift
@@ -9,19 +9,70 @@ struct Settings: View {
     var body: some View {
         NavigationView {
             ZStack {
-                Image(systemName: "slider.horizontal.3")
-                    .foregroundColor(Color.white)
-                    .font(.system(size: 100.0))
+                List {
+                    Section(header: HStack {
+                        Image(systemName: "questionmark.circle")
+                        Text("Support")
+                        
+                    }) {
+                        HStack {
+                            Image(systemName: "book")
+                            Text("User manual")
+                        }
+                        HStack {
+                            Image(systemName: "phone")
+                            Text("Call us")
+                        }
+                        HStack {
+                            Image(systemName: "envelope")
+                            Text("Email us")
+                        }
+                    }
+                    .headerProminence(.increased)
+                    
+                    Section(header: HStack {
+                        Image(systemName: "shield.lefthalf.fill")
+                        Text("Legal Policy")
+                        
+                    }, footer: Text("For more legal policy please contact us.")) {
+                        HStack {
+                            Image(systemName: "doc.plaintext")
+                            Text("Terms and conditions")
+                        }
+                        HStack {
+                            Image(systemName: "lock.shield")
+                            Text("Privacy Policy")
+                        }
+                    }
+                    .headerProminence(.increased)
+                    
+                    Section(header: HStack {
+                        Image(systemName: "exclamationmark.shield")
+                        Text("About the app")
+                        
+                    }, footer: HStack {
+                        Image(systemName: "r.circle")
+                        Text("All rights reserved")
+                    }) {
+                        HStack {
+                            Image(systemName: "bag")
+                            Text("Version: 0.0.0")
+                        }
+                        HStack {
+                            Image(systemName: "calendar.circle")
+                            Text("Last update: 17.04.2022")
+                        }
+                        HStack {
+                            Image(systemName: "square.and.pencil")
+                            Text("Developed by: Filip Kjamilov")
+                        }
+                    }
+                    .headerProminence(.increased)
+                }
+                .listStyle(InsetGroupedListStyle())
                 
             }
             .navigationBarTitle("Settings", displayMode: .large)
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(
-                Image("background")
-                    .resizable()
-                    .scaledToFill()
-                    .ignoresSafeArea(.all)
-            )
         }
     }
 }
