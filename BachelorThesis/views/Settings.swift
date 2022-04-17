092c4618b80bc2a3be26ef6f209506b2134811fa
Filip Kjamilov <filip.kjamilov@netcetera.com>
2022-04-17 14:15:01 +0200

    [no-issue] MOD structure settings view

--- BachelorThesis/views/Settings.swift
+++ BachelorThesis/views/Settings.swift
@@ -10,70 +10,85 @@ struct Settings: View {
         NavigationView {
             ZStack {
                 List {
-                    Section(header: HStack {
-                        Image(systemName: "questionmark.circle")
-                        Text("Support")
-                        
-                    }) {
-                        HStack {
-                            Image(systemName: "book")
-                            Text("User manual")
-                        }
-                        HStack {
-                            Image(systemName: "phone")
-                            Text("Call us")
-                        }
-                        HStack {
-                            Image(systemName: "envelope")
-                            Text("Email us")
-                        }
-                    }
-                    .headerProminence(.increased)
-                    
-                    Section(header: HStack {
-                        Image(systemName: "shield.lefthalf.fill")
-                        Text("Legal Policy")
-                        
-                    }, footer: Text("For more legal policy please contact us.")) {
-                        HStack {
-                            Image(systemName: "doc.plaintext")
-                            Text("Terms and conditions")
-                        }
-                        HStack {
-                            Image(systemName: "lock.shield")
-                            Text("Privacy Policy")
-                        }
-                    }
-                    .headerProminence(.increased)
-                    
-                    Section(header: HStack {
-                        Image(systemName: "exclamationmark.shield")
-                        Text("About the app")
-                        
-                    }, footer: HStack {
-                        Image(systemName: "r.circle")
-                        Text("All rights reserved")
-                    }) {
-                        HStack {
-                            Image(systemName: "bag")
-                            Text("Version: 0.0.0")
-                        }
-                        HStack {
-                            Image(systemName: "calendar.circle")
-                            Text("Last update: 17.04.2022")
-                        }
-                        HStack {
-                            Image(systemName: "square.and.pencil")
-                            Text("Developed by: Filip Kjamilov")
-                        }
-                    }
-                    .headerProminence(.increased)
+                    SupportView()
+                    LegalPolicyView()
+                    AboutTheAppView()
                 }
                 .listStyle(InsetGroupedListStyle())
-                
             }
             .navigationBarTitle("Settings", displayMode: .large)
         }
+    }
+}
+
+struct SupportView: View {
+    var body: some View {
+        Section(header: HStack {
+            Image(systemName: "questionmark.circle")
+            Text("Support")
+            
+        }) {
+            HStack {
+                Image(systemName: "book")
+                Text("User manual")
+            }
+            HStack {
+                Image(systemName: "phone")
+                Text("Call us")
+            }
+            HStack {
+                Image(systemName: "envelope")
+                Text("Email us")
+            }
+        }
+        .headerProminence(.increased)
+    }
+}
+
+struct LegalPolicyView: View {
+    var body: some View {
+        Section(header: HStack {
+            Image(systemName: "shield.lefthalf.fill")
+            Text("Legal Policy")
+            
+        }, footer: Text("For more legal policy please contact us.")) {
+            HStack {
+                Image(systemName: "doc.plaintext")
+                Text("Terms and conditions")
+            }
+            HStack {
+                Image(systemName: "lock.shield")
+                Text("Privacy Policy")
+            }
+        }
+        .headerProminence(.increased)
+    }
+}
+
+struct AboutTheAppView: View {
+    var body: some View {
+        Section(header: HStack {
+            Image(systemName: "exclamationmark.shield")
+            Text("About the app")
+            
+        }, footer: HStack {
+            Image(systemName: "r.circle")
+            Text("All rights reserved")
+        }) {
+            HStack {
+                Image(systemName: "bag")
+                Text("Version: 0.0.0")
+            }
+            HStack {
+                Image(systemName: "calendar.circle")
+                Text("Last update: 17.04.2022")
+            }
+            HStack {
+                Image(systemName: "square.and.pencil")
+                Text("Developed by: Filip Kjamilov")
+            }
+        }
+        .headerProminence(.increased)
     }
 }
 
