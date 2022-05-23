748cb6a8b5f037f6322de31487260dcb0a50b9e1
Filip Kjamilov <filip.kjamilov@netcetera.com>
2022-05-23 19:30:17 +0200

    [no-issue] RENAME app and add initial appicon

---
+++ WalletLess/views/MailView.swift
@@ -0,0 +1,56 @@
+//  Created by Filip Kjamilov on 17.4.22.
+
+import Foundation
+import SwiftUI
+import UIKit
+import MessageUI
+
+struct MailView: UIViewControllerRepresentable {
+    
+    @Environment(\.presentationMode) var presentation
+    @Binding var result: Result<MFMailComposeResult, Error>?
+    
+    let newSubject: String
+    let newMessageBody: String
+    
+    class MailCoordinator: NSObject, MFMailComposeViewControllerDelegate {
+        @Binding var presentation: PresentationMode
+        @Binding var result: Result<MFMailComposeResult, Error>?
+        
+        init(presentation: Binding<PresentationMode>,
+             result: Binding<Result<MFMailComposeResult, Error>?>) {
+            _presentation = presentation
+            _result = result
+        }
+        
+        func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+            defer {
+                $presentation.wrappedValue.dismiss()
+            }
+            guard error == nil else {
+                self.result = .failure(error!)
+                return
+            }
+            self.result = .success(result)
+        }
+    }
+    
+    func makeCoordinator() -> MailCoordinator {
+        return MailCoordinator(presentation: presentation,
+                               result: $result)
+    }
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<MailView>) -> MFMailComposeViewController {
+        let viewController = MFMailComposeViewController()
+        viewController.mailComposeDelegate = context.coordinator
+        viewController.setToRecipients(["support@walletless.com"])
+        viewController.setSubject(newSubject)
+        viewController.setMessageBody(newMessageBody, isHTML: false)
+        return viewController
+    }
+    
+    func updateUIViewController(_ uiViewController: MFMailComposeViewController,
+                                context: UIViewControllerRepresentableContext<MailView>) {
+        
+    }
+}
